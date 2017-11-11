6508051f8c13638ccd3300b472ea1c8cd5fdd814
Raymundo Villanueva <raymundovillanueva@Raymundos-MacBook-Pro.local>
2017-11-11 13:04:12 -0500

    node working

--- server.js
+++ server.js
@@ -8,6 +8,42 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var user =[];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+  app.get("/table", function(req, res) {
+    res.sendFile(path.join(__dirname, "table.html"));
+  });
+  
+  app.get("/reservation", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+  });
+  app.get("/all", function(req, res) {
+    res.json(user);
+  });
+
+
+app.post("/reservation", function(req, res) {
+    var newReservation = req.body;
+
+    //newReservation.uniqueId;
+    // newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    // console.log(newcharacter);
+
+    console.log(newReservation);
+  
+    user.push(newReservation);
+  
+    res.json(newReservation);
+
+  });
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+
